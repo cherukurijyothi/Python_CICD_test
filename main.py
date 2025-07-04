0a54b1eb4d048edffae72d6025c7b683064b147c
Jyothi Cherukuri <koti.jyothi29@gmail.com>
2025-07-04 19:00:54 -0400

    Added commit 3

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Test from FastAPI from Jenkins to server "}
+    return {"message": "Test from FastAPI from Jenkins to server -123"}
